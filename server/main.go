5f2a182e3c537896a4762a09691930d3d6e26004
Owen Strength <Owenstrength@gmail.com>
2024-02-29 15:22:42 -0600

    made config secret

--- server/main.go
+++ server/main.go
@@ -5,9 +5,9 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/gorilla/mux"
-	"github.com/owenstrength/vinyl-ify/server/config"
 )
 
 func main() {
@@ -17,18 +17,18 @@ func main() {
 	r.HandleFunc("/login", handleLogin).Methods("GET")
 
 	// Start the server
-	port := ":8080"
+	port := ":8000"
 	fmt.Printf("Server listening on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, r))
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	// Load Spotify configuration from local file
-	cfg := config.LoadConfig("config.json")
+	cfg := LoadConfig()
+	fmt.Println(cfg)
 
 	// Construct the Spotify authorization URL
 	authURL := fmt.Sprintf("https://accounts.spotify.com/authorize?client_id=%s&response_type=code&redirect_uri=%s&scope=%s",
-		cfg.SpotifyClientID, cfg.SpotifyRedirectURI, url.QueryEscape(Scopes))
+		cfg.SpotifyClientID, cfg.SpotifyRedirectURI, url.QueryEscape(strings.Join(cfg.Scopes[:], " ")))
 
 	// Redirect the user to the Spotify authorization URL
 	http.Redirect(w, r, authURL, http.StatusTemporaryRedirect)
